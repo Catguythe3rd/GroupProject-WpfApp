f1d0c0e5d5c746fc85bd4241f5d46ac753b8b543
Anastasia Armstrong <aarmstrong1@mail.weber.edu>
2023-11-14 16:49:57 -0700

    items and invoice list + some classes

--- Main/clsMainLogic.cs
+++ Main/clsMainLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GroupProject_WpfApp.Items;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,36 @@ namespace GroupProject_WpfApp.Main
 {
     internal class clsMainLogic
     {
+        static List<clsItem> items; // some sort of list created from sql database.
+        static List<clsSearch> invoices; //list of invoices
+
+        #region items
+
+        bool isValidCost;
+        bool isValidName;
+        bool isValidDescription;
+        clsMainLogic()
+        {
+            items = new List<clsItem>(); // create new list
+        }
+
+
+        private void saveInvoice()
+        {
+            // Take in selected item ID from SQL database
+            //save into new sql invoice table.
+        }
+
+        void deleteItem()
+        {
+            // delete from invoice only
+        }
+        #endregion
+
+        #region search
+        #endregion
+
+        #region main
+        #endregion   
     }
 }
