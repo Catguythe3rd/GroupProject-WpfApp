34bb11b637820c22440875f12d3661039cbb7886
Catguythe3rd <mathieuandlebaron@gmail.com>
2023-12-08 18:37:02 -0700

    Buttons are functional but untested

--- Items/wndItems.xaml.cs
+++ Items/wndItems.xaml.cs
@@ -31,8 +31,12 @@ namespace GroupProject_WpfApp.Items
         clsItem? selectedItem;           // The cureently selected item in the list.
         List<clsItem> itemsList;        // list of jewelery items
 
-        bool userHasChangedItemCodeTextBox = false;
+        bool isValid_Code = false;
+        bool isValid_Description = false;
+        bool isValid_Cost = false;
+
         bool userHasEnteredNewItemCode = false;
+
         #endregion
 
         /// <summary>
@@ -50,57 +54,11 @@ namespace GroupProject_WpfApp.Items
                 itemsTable_DataGrid.ItemsSource = itemsList;        // Sets the datagrid to the list of items.
                 EditError_Label.Visibility = Visibility.Collapsed;  // Closes error box before starting the program.
 
+                add_Button.IsEnabled = false;
                 save_Button.IsEnabled = false;
                 delete_Button.IsEnabled = false;
-            }
-            catch (Exception ex)
-            {
-                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
-                    MethodInfo.GetCurrentMethod().Name, ex.Message);
-            }
-        }
-
-        private void add_Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                clsItemsLogic.insertItem(Edit_Code_TextBox.Text, Edit_Description_TextBox.Text, Decimal.Parse(Edit_Cost_TextBox.Text));
-            }
-            catch (Exception ex)
-            {
-                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
-                    MethodInfo.GetCurrentMethod().Name, ex.Message);
-            }
-        }
-
-        private void delete_Button_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                if (selectedItem == null)
-                {
-                    // error label
-                }
-                else
-                {
-                    // must check if selected item is in an existing invoice, if so don't allow delete, update error label.
-                    clsItemsLogic.deleteItem(selectedItem.ItemCode); // Deletes item from database.
-                    itemsList = clsItemsLogic.getAllItems();         // Reloads item list from data base.
-                    itemsTable_DataGrid.ItemsSource = itemsList;     // Reloads datagrid with items from list.
-                    selectedItem = null;                             // Sets slected item to default.
-
-                    // Sets the values in the Edit Items group box to initial null values.
-                    Edit_Code_TextBox.Text = null;
-                    Edit_Description_TextBox.Text = null;
-                    Edit_Cost_TextBox.Text = null; // cost is in decimal, so converts to string.
-
-                    // Disables save and delete buttons
-                    save_Button.IsEnabled = false;
-                    delete_Button.IsEnabled = false;
-
-                    // Collapses error label
-                    EditError_Label.Visibility = Visibility.Collapsed;
-                }
+                upArrow_Button.IsEnabled = false;
+                downArrow_Button.IsEnabled = false;
             }
             catch (Exception ex)
             {
@@ -118,10 +76,15 @@ namespace GroupProject_WpfApp.Items
         {
             try
             {
-                if (selectedItem != null) // Checks if an item has been selected from the list.
+                // If there's no selected item, but there are items in the datagrid, select the top most one.
+                if (selectedItem == null && itemsTable_DataGrid != null)
+                {
+                    itemsTable_DataGrid.SelectedIndex = 0;
+                }
+                else // Iterate down one in the datagrid.
                 {
                     // Won't select a higher item if it's at the top of the list.
-                    if (itemsTable_DataGrid.SelectedIndex != 0) 
+                    if (itemsTable_DataGrid.SelectedIndex != 0)
                     {
                         itemsTable_DataGrid.SelectedIndex -= 1;
                     }
@@ -143,14 +106,75 @@ namespace GroupProject_WpfApp.Items
         {
             try
             {
-                if (selectedItem != null) // Checks if an item has been selected from the list.
+                // If there's no selected item, but there are items in the datagrid, select the bottom most one.
+                if (selectedItem == null && itemsTable_DataGrid != null)
+                {
+                    itemsTable_DataGrid.SelectedIndex = itemsList.Count - 1;
+                }
+                else // Iterate down one in the datagrid.
                 {
                     // Won't select a lower item if it's at the bottom of the list.
-                    if (itemsTable_DataGrid.SelectedIndex != itemsTable_DataGrid.Items.Count - 1) 
+                    if (itemsTable_DataGrid.SelectedIndex != itemsTable_DataGrid.Items.Count - 1)
                     {
                         itemsTable_DataGrid.SelectedIndex += 1;
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void add_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                clsItemsLogic.insertItem((string)Edit_Code_TextBox.Text, (string)Edit_Description_TextBox.Text, Decimal.Parse(Edit_Cost_TextBox.Text));
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                clsItemsLogic.updateItem(selectedItem.ItemDesc, selectedItem.Cost, selectedItem.ItemCode);
+                EditError_Label.Visibility = Visibility.Collapsed;
+            }
+            catch (Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // must check if selected item is in an existing invoice, if so don't allow delete, update error label.
+                clsItemsLogic.deleteItem(selectedItem.ItemCode); // Deletes item from database.
+                itemsList = clsItemsLogic.getAllItems();         // Reloads item list from data base.
+                itemsTable_DataGrid.ItemsSource = itemsList;     // Reloads datagrid with items from list.
+                selectedItem = null;                             // Sets slected item to default.
+
+                // Sets the values in the Edit Items group box to initial null values.
+                Edit_Code_TextBox.Text = null;
+                Edit_Description_TextBox.Text = null;
+                Edit_Cost_TextBox.Text = null; // cost is in decimal, so converts to string.
+
+                // Disables save and delete buttons
+                save_Button.IsEnabled = false;
+                delete_Button.IsEnabled = false;
+
+                // Collapses error label
+                EditError_Label.Visibility = Visibility.Collapsed;
             }
             catch (Exception ex)
             {
@@ -175,9 +199,6 @@ namespace GroupProject_WpfApp.Items
                 Edit_Code_TextBox.Text = selectedItem.ItemCode;
                 Edit_Description_TextBox.Text = selectedItem.ItemDesc;
                 Edit_Cost_TextBox.Text = selectedItem.Cost.ToString(); // cost is in decimal, so converts to string.
-
-                // Since new info has loaded into the cost textbox, the saved values haven't been changed by the user.
-                userHasChangedItemCodeTextBox = false;
             }
             catch (Exception ex)
             {
@@ -186,34 +207,133 @@ namespace GroupProject_WpfApp.Items
             }
         }
 
-        private void Save_Button_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// When the testbox is changed, it checks whether the item code has been changed by the user.
+        /// If its a different code than one in the list, then the user can create a new item.
+        /// If its the same code as one in the list, then the user can update the corresponding item.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Edit_Code_TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
             {
-                clsItemsLogic.updateItem(selectedItem.ItemDesc, selectedItem.Cost, selectedItem.ItemCode);
-                EditError_Label.Visibility = Visibility.Collapsed;
+                if (string.IsNullOrEmpty(Edit_Code_TextBox.Text) == true)
+                {
+                    isValid_Code = false;
+                }
+                else
+                {
+                    isValid_Code = true;
+                    userHasEnteredNewItemCode = false;
+
+                    // Iterates through list and checks if the users itemCode matches an already existing code.
+                    for (int i = 0; i < itemsList.Count; i++)
+                    {
+                        if (Edit_Code_TextBox.Text == itemsList[i].ItemCode)
+                        {
+                            userHasEnteredNewItemCode = true;
+                            selectedItem = itemsList[i];
+                            itemsTable_DataGrid.SelectedItem = selectedItem;
+                            break;
+                        }
+                    }
+                }
+
+                updateEditItemsButtons();
             }
-            catch (Exception ex)
+            catch (System.Exception ex)
             {
                 HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
-                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
             }
         }
-        
-        /*
 
-        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        private void Edit_Description_TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
             try
             {
+                if (string.IsNullOrEmpty(Edit_Description_TextBox.Text) == true)
+                {
+                    isValid_Description = false;
+                }
+                else
+                {
+                    isValid_Description = true;
+                }
 
+                updateEditItemsButtons();
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void Edit_Cost_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Edit_Cost_TextBox.Text) == true)
+                {
+                    isValid_Cost = false;
+                }
+                else
+                {
+                    isValid_Cost = true;
+                }
+
+                updateEditItemsButtons();
+            }
+            catch (System.Exception ex)
+            {
+                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
+                            MethodInfo.GetCurrentMethod().Name, ex.Message);
+            }
+        }
+
+        private void updateEditItemsButtons()
+        {
+            try
+            {
+                // Delete
+                if (isValid_Code == true && userHasEnteredNewItemCode == false)
+                {
+                    delete_Button.IsEnabled = true;
+                }
+                else
+                {
+                    delete_Button.IsEnabled = false;
+                }
+
+                // Save
+                if (isValid_Code == true && isValid_Description == true && isValid_Cost == true 
+                    && userHasEnteredNewItemCode == false)
+                {
+                    save_Button.IsEnabled = true;
+                }
+                else
+                {
+                    save_Button.IsEnabled = false;
+                }
+
+                // Add
+                if(isValid_Code == true && isValid_Description == true && isValid_Cost == true 
+                    && userHasEnteredNewItemCode == true)
+                {
+                    add_Button.IsEnabled = true;
+                }
+                else
+                {
+                    add_Button.IsEnabled= false;
+                }
             }
             catch (Exception ex)
             {
-                HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
-                    MethodInfo.GetCurrentMethod().Name, ex.Message);
+                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
             }
-        }*/
+        }
 
         private void txtLetterInput_PreviewKeyDown(object sender, KeyEventArgs e)
         {
@@ -223,7 +343,7 @@ namespace GroupProject_WpfApp.Items
                 if (!(e.Key >= Key.A && e.Key <= Key.Z))
                 {
                     //Allow the user to use the backspace, delete, tab and enter
-                    if (!(e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Tab || e.Key == Key.Enter 
+                    if (!(e.Key == Key.Back || e.Key == Key.Delete || e.Key == Key.Tab || e.Key == Key.Enter
                         || e.Key == Key.Space))
                     {
                         //No other keys allowed besides numbers, backspace, delete, tab, and enter
@@ -260,71 +380,18 @@ namespace GroupProject_WpfApp.Items
             }
         }
 
-        /// <summary>
-        /// When the testbox is changed, it checks whether the item code has been changed by the user.
-        /// If its a different code than one in the list, then the user can create a new item.
-        /// If its the same code as one in the list, then the user can update the corresponding item.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void Edit_Code_TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        // Returns the selected Item to main when the window closes.
+        private void itemWindow_Close(object sender, CancelEventArgs e)
         {
             try
             {
-                if (userHasChangedItemCodeTextBox == false)
-                {
-                    userHasEnteredNewItemCode = true;   // Is true by default,
-                                                        // if a duplicate is found in the list, its set to false.
-
-                    for (int i = 0; i < itemsList.Count; i++)
-                    {
-                        if (Edit_Code_TextBox.Text == itemsList[i].ItemCode)
-                        {
-                            userHasEnteredNewItemCode = false;
-                            break;
-                        }
-                    }
-                }
-
-                //UpdateErrorLabel();
+                parentWindow.itemID = selectedItem;
             }
             catch (System.Exception ex)
             {
                 HandleError(MethodInfo.GetCurrentMethod().DeclaringType.Name,
                             MethodInfo.GetCurrentMethod().Name, ex.Message);
             }
-        }
-
-        /*public void UpdateErrorLabel()
-        {
-            try
-            {
-                if (userHasEnteredNewItemCode == true)
-                {
-                    EditError_Label.Visibility = Visibility.Visible;
-                    EditError_Label.Text = "Warning: ID code can only be changed when creating a new Item.";
-                    save_Button.IsEnabled = false;
-                    add_Button.IsEnabled = true;
-                }
-                else if (userHasEnteredNewItemCode == false)
-                {
-                    EditError_Label.IsEnabled = true;
-                    EditError_Label.Visibility = Visibility.Visible;
-                    EditError_Label.Text = "Warning: ID code can only be changed when creating a new Item.";
-                    save_Button.IsEnabled = true;
-                    add_Button.IsEnabled = false;
-                }
-            }
-            catch (Exception ex)
-            {
-                throw new Exception(MethodInfo.GetCurrentMethod().DeclaringType.Name + "." + MethodInfo.GetCurrentMethod().Name + "->" + ex.Message);
-            }
-        }*/
-
-        // Returns the selected Item to main when the window closes.
-        private void itemWindow_Close(object sender, CancelEventArgs e)
-        {
-            parentWindow.itemID = selectedItem;
         }
 
         private void HandleError(string sClass, string sMethod, string sMessage)
